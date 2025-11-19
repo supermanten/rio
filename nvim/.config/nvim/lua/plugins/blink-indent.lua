13de14ed1914e6a66f1c5401d532b09030d51552
rio <rio353@gamil.com>
2025-11-19 21:41:53 +0530

    add blink indent

---
+++ nvim/.config/nvim/lua/plugins/blink-indent.lua
@@ -0,0 +1,8 @@
+return {
+	"saghen/blink.indent",
+	version = "*",
+	dependencies = "saghen/blink.download",
+	event = { "BufReadPost", "BufNewFile" },
+	-- @module 'blink.indent',
+	-- @type 'blink.indent.Config'
+}
